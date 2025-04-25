1498635d0c5ec7a907cf51c0ad34415d3523f724
49Harsh <49harshyadav@gmail.com>
2025-04-25 10:25:31 +0530

    fix  react is not define

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#6c47ff',
+        'primary-light': 'rgba(108, 71, 255, 0.2)',
+        'primary-hover': '#5a3cd7',
+      },
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
